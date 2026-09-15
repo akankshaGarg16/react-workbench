3137c17bfcc81b2203f65857ec31751f971b5164
Akanksha <garg.akansha1600@gmail.com>
2026-09-15 13:02:26 +0530

    infinite scroll logic added

--- 14-Infinite-scrolls+Shimmer-UI/src/App.jsx
+++ 14-Infinite-scrolls+Shimmer-UI/src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import Memes from './Memes'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
    <div><Memes /></div>
   )
--- 14-Infinite-scrolls+Shimmer-UI/src/Memes.jsx
+++ 14-Infinite-scrolls+Shimmer-UI/src/Memes.jsx
@@ -3,26 +3,41 @@ import MemeCard from "./MemeCard";
 import Shimmer from "./Shimmer";
 
 const Memes = () => {
-  const [memes, setMemes] = useState(null);
+  const [memes, setMemes] = useState([]);
+  const [showShimmer, setShowShimmer] = useState(false);
   useEffect(() => {
     fetchMemes();
+
+    window.addEventListener("scroll", handleScroll)
+    
+    return () => window.removeEventListener("scroll", handleScroll)
   }, []);
 
+  function handleScroll() {
+    // scrollY: how much we've scrolled
+    // innerHeight: height of the window(visible section)
+    // document.body.scrollHeight: total height of the web page
+    if (window.scrollY + window.innerHeight >= document.body.scrollHeight) {
+      fetchMemes();
+    }
+    }
+
   const fetchMemes = async () => {
+    setShowShimmer(true);
     const res = await fetch("https://meme-api.com/gimme/20");
     const json = await res.json();
     console.log(json);
-    setMemes(json.memes);
+    setShowShimmer(false);
+    setMemes((memes) => [...memes, ...json.memes]);
   };
   return (
     <div className="flex flex-wrap">
-      {!memes ? (
-        <Shimmer />
-      ) : (
+      {
         memes.map((meme, index) => {
           return <MemeCard key={index} memeData={meme} />;
         })
-      )}
+      }
+      {showShimmer &&  <Shimmer />}
     </div>
   );
 };
